91aa1b661d3862e0b921faa74135e8b6b7d1f374
Roger <Worth22Focus3000@gmail.com>
2021-08-07 17:08:35 +0800

    完成後台首頁各項卡片的資訊

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -25,11 +25,25 @@
       <ToastMessages />
       <router-view
         class="admin-page-products"
+        :products-length="productsLength"
+        :orders-length="ordersData.ordersNum"
+        :coupons-length="couponsData.couponsNum"
+        :total-income="totalIncome"
         @change-navbar-page-title="changeNavbarPageTitle"
         @change-navbar-btn-title="changeNavbarBtnTitle"
         @change-current-page="changeCurrentPage"
       ></router-view>
     </div>
+    <!-- Loading -->
+    <Loading
+      :active="isLoading"
+      :background-color="loadingObj.bgc"
+      :loader="loadingObj.style"
+      :color="loadingObj.color"
+      :opacity="loadingObj.opacity"
+      :height="loadingObj.height"
+      :width="loadingObj.width"
+    ></Loading>
   </div>
 </template>
 
@@ -38,8 +52,10 @@ import emitter from "@/methods/emitter"; // 引入mitt套件(類似Vue2 Event Bu
 import ToastMessages from "../components/ToastMessages.vue"; // 引入提示訊息元件
 import Sidebar from "../components/adminPages/Sidebar.vue";
 import Navbar from "../components/adminPages/Navbar.vue";
+import LoadingConfiguration from "../mixins/LoadingConfiguration";
 
 export default {
+  mixins: [LoadingConfiguration],
   components: {
     ToastMessages,
     Sidebar,
@@ -54,13 +70,30 @@ export default {
   data() {
     return {
       sidebarStatus: false, // Sidebar元件的樣式開關
+      currentPage: "AdminHome", // Sidebar 當前顯示頁面
       navbarPageTitle: "", // Navbar 頁面標題
       navbarOrderBtnTitle: "", // Navbar 按鈕文字
-      currentPage: 'AdminHome', // 當前頁面
+      productsLength: 0, // 目前庫存商品數
+
+      ordersData: {
+        page: 1, // 當前API的分頁頁數
+        pageNum: 0, // 當前API分頁的訂單數
+        ordersArray: [], // 訂單總陣列
+        ordersNum: 0, // 訂單總數
+      },
+
+      couponsData: {
+        page: 1, // 當前API的分頁頁數
+        pageNum: 0, // 當前API分頁的訂單數
+        couponsArray: [], // 訂單總陣列
+        couponsNum: 0, // 訂單總數
+      },
+
+      totalIncome: 0, // 目前的總獲利
     };
   },
   methods: {
-    // 更改當前頁面樣式
+    // 更改Sidebar分頁
     changeCurrentPage(name) {
       this.currentPage = name;
     },
@@ -90,10 +123,125 @@ export default {
         }
       });
     },
+    // 取得目前庫存商品數量
+    getProducts() {
+      this.isLoading = true;
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/products/all`;
+
+      this.$http.get(api).then((res) => {
+        if (res.data.success) {
+          this.productsLength = Object.keys(res.data.products).length;
+          this.isLoading = false;
+        }
+      });
+    },
+    // 取得當前分頁的訂單數量
+    getOrders() {
+      this.isLoading = true;
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/orders?page=${this.ordersData.page}`;
+
+      this.$http.get(api).then((res) => {
+        if (res.data.success) {
+          // 組合並儲存訂單總陣列
+          this.ordersData.ordersArray = this.ordersData.ordersArray.concat(
+            res.data.orders
+          );
+          // 當前分頁的訂單數量
+          this.ordersData.pageNum = res.data.orders.length;
+          // 訂單API判斷函式
+          this.ordersAPIManager();
+        } else {
+          // 此處若失敗，可能是沒有訂單了。直接顯示已儲存的陣列數量
+          this.ordersData.ordersNum = this.ordersData.ordersArray.length;
+          // 計算出目前總獲利
+          this.getTotalIncome();
+          this.isLoading = false;
+        }
+      });
+    },
+    // 訂單API判斷函式
+    ordersAPIManager() {
+      if (this.ordersData.pageNum === 10) {
+        this.ordersData.page += 1;
+        this.getOrders();
+      } else {
+        // 此處若失敗，可能是沒有訂單了。直接顯示已儲存的陣列數量
+        this.ordersData.ordersNum = this.ordersData.ordersArray.length;
+        // 計算出目前總獲利
+        this.getTotalIncome();
+        this.isLoading = false;
+      }
+    },
+    // 取得當前分頁的優惠券數量
+    getCoupons() {
+      this.isLoading = true;
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/coupons?page=${this.couponsData.page}`;
+
+      this.$http.get(api).then((res) => {
+        if (res.data.success) {
+          // 組合並儲存訂單總陣列
+          this.couponsData.couponsArray = this.couponsData.couponsArray.concat(
+            res.data.coupons
+          );
+          // 當前分頁的訂單數量
+          this.couponsData.pageNum = res.data.coupons.length;
+          // 訂單API判斷函式
+          this.couponsAPIManager();
+        } else {
+          // 此處若失敗，可能是沒有訂單了。直接顯示已儲存的陣列數量
+          this.couponsData.couponsNum = this.couponsData.couponsArray.length;
+          this.isLoading = false;
+        }
+      });
+    },
+    // 優惠券API判斷函式
+    couponsAPIManager() {
+      if (this.couponsData.pageNum === 10) {
+        this.couponsData.page += 1;
+        this.getCoupons();
+      } else {
+        // 此處若失敗，可能是沒有訂單了。直接顯示已儲存的陣列數量
+        this.couponsData.couponsNum = this.couponsData.couponsArray.length;
+        this.isLoading = false;
+      }
+    },
+    // 取得 目前已付款訂單的總獲利
+    getTotalIncome() {
+      this.isLoading = true;
+      if (this.ordersData.ordersNum > 0) {
+        this.totalIncome = this.ordersData.ordersArray
+          // 篩選出 已付款訂單
+          .filter((order) => {
+            return order.is_paid;
+          })
+          // 將訂單的總付款金額，重組成一個新的陣列
+          .map((order) => {
+            return order.total;
+          })
+          // 將陣列中的值，全部相加起來。
+          .reduce((a, b) => {
+            return a + b;
+          })
+          // 將總金額做四捨五入
+          .toFixed(0);
+      } else {
+        this.totalIncome = 0;
+      }
+
+      this.isLoading = false;
+    },
   },
   created() {
+    // 驗證登入狀態
     this.loginVerification();
+    // 取得當前頁面(適用於剛登入進來、重新整理)
     this.currentPage = this.$route.name;
+    // 取得庫存商品總數
+    this.getProducts();
+    // 取得新增訂單總數
+    this.getOrders();
+    // 取得庫存優惠券總數
+    this.getCoupons();
   },
 };
 </script>
--- src/views/adminPages/Home.vue
+++ src/views/adminPages/Home.vue
@@ -28,7 +28,7 @@
               <i class="bi bi-cart3"></i>
             </div>
             <div class="card-content">
-              <div class="number">{{ orderList?.length }}</div>
+              <div class="number">{{ ordersLength }}</div>
               <div class="title">新增訂單</div>
             </div>
           </div>
@@ -66,7 +66,7 @@
               <i class="bi bi-bank"></i>
             </div>
             <div class="card-content">
-              <div class="number">${{ $filters.currency(getTotalIncome) }}</div>
+              <div class="number">${{ $filters.currency(totalIncome) }}</div>
               <div class="title">總獲利額</div>
             </div>
           </div>
@@ -96,67 +96,38 @@ export default {
     ProductsRankingChart,
     OrdersChart,
   },
+  props: {
+    productsLength: {
+      type: Number,
+      default() {
+        return 0;
+      },
+    },
+    ordersLength: {
+      type: Number,
+      default() {
+        return 0;
+      },
+    },
+    couponsLength: {
+      type: Number,
+      default() {
+        return 0;
+      },
+    },
+    totalIncome: {
+      type: Number,
+      default() {
+        return 0;
+      },
+    },
+  },
   // 使用父元件的 emitter元件
   inject: ["emitter"],
   data() {
-    return {
-      productsLength: 0, // 庫存商品數量
-      orderList: [], // API訂單資料
-      couponsLength: 0, // 優惠券數量
-    };
-  },
-  computed: {
-    // 取得 總獲利的值
-    getTotalIncome() {
-      if (this.orderList.length > 0) {
-        return this.orderList
-          .filter((order) => {
-            return order.is_paid;
-          })
-          .map((order) => {
-            return order.total;
-          })
-          .reduce((a, b) => {
-            return a + b;
-          });
-      } else {
-        return 0;
-      }
-    },
+    return {};
   },
   methods: {
-    // 取得庫存商品數量
-    getProducts() {
-      this.isLoading = true;
-      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/products/all`;
-
-      this.$http.get(api).then((res) => {
-        if (res.data.success) {
-          this.productsLength = Object.keys(res.data.products).length;
-        }
-      });
-    },
-    // 取得新增訂單數量
-    getOrders(page = 1) {
-      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/orders?page=${page}`;
-
-      this.$http.get(api).then((res) => {
-        if (res.data.success) {
-          this.orderList = res.data.orders;
-        }
-      });
-    },
-    // 取得優惠券數量
-    getCoupons(page = 1) {
-      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/coupons?page=${page}`;
-
-      this.$http.get(api).then((res) => {
-        if (res.data.success) {
-          this.couponsLength = res.data.coupons.length;
-          this.isLoading = false;
-        }
-      });
-    },
     // 返回前台首頁
     goUserHomePage() {
       // 使用者提示訊息
@@ -184,12 +155,6 @@ export default {
     this.$emit("change-navbar-page-title", "資訊儀表板");
     // 更改Navbar表單按鈕標題
     this.$emit("change-navbar-btn-title", "回前台首頁");
-    // 取得庫存商品數
-    this.getProducts();
-    // 取得訂單數量
-    this.getOrders();
-    // 取得優惠券數量
-    this.getCoupons();
     // 監聽Navbar的 新增按鈕
     this.emitter.on("open-modal", (status) => {
       if (status === "資訊儀表板") {
