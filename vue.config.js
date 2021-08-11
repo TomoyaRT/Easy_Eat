67574bdda1126706515530ff2f6edf21285be3d0
Roger <Worth22Focus3000@gmail.com>
2021-08-11 15:30:40 +0800

    修改網頁標題

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Easy Eat商城';
+        return args;
+      });
+  },
   // 部屬專案的 URL
   publicPath: '/Easy_Eat/dist/'
 }
